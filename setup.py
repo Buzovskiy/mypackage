9b11fc9729896cc16f9b9e27612c0826fb17f383
Vitaliy Buzovskiy <buzovskiy@gmail.com>
2021-07-31 17:05:15 +0300

    change package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mypackage-buzovskyi-vitalii", # Replace with your own username
+    name="mypackage-osar", # Replace with your own username
     version="0.0.1",
     author="Buzovskyi Vitalii",
     author_email="buzovskiy.v@gmail.com",
