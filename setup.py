c9cbfbbd18e0e8efbf40b28c1b18a1d826a5ea73
Vitaliy Buzovskiy <buzovskiy@gmail.com>
2021-07-31 18:56:23 +0300

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mypackage-osar", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Buzovskyi Vitalii",
     author_email="buzovskiy.v@gmail.com",
     description="test mypackage",
