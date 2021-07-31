f3dd7fae296b31f3d24e72626a0c4295e48da406
Vitaliy Buzovskiy <buzovskiy@gmail.com>
2021-07-31 19:39:25 +0300

    add multiply v0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mypackage-osar", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="Buzovskyi Vitalii",
     author_email="buzovskiy.v@gmail.com",
     description="test mypackage",
